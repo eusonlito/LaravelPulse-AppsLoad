c3f1d69f5ec8d9fc75b5eaa69d10d2c4c1d064bc
Lito <lito@eordes.com>
2024-02-05 13:37:59 +0100

    Improved Card Layout

--- resources/views/livewire/card.blade.php
+++ resources/views/livewire/card.blade.php
@@ -16,43 +16,43 @@
 
         @else
 
-            <x-pulse::table>
-                <colgroup>
-                    <col width="100%" />
-                    <col width="0%" />
-                    <col width="0%" />
-                </colgroup>
+        <x-pulse::table>
+            <colgroup>
+                <col width="100%" />
+                <col width="0%" />
+                <col width="0%" />
+            </colgroup>
 
-                <x-pulse::thead>
-                    <tr>
-                        <x-pulse::th>App</x-pulse::th>
-                        <x-pulse::th class="text-right">Memory</x-pulse::th>
-                        <x-pulse::th class="text-right">CPU</x-pulse::th>
-                    </tr>
-                </x-pulse::thead>
+            <x-pulse::thead>
+                <tr>
+                    <x-pulse::th>App</x-pulse::th>
+                    <x-pulse::th class="text-right">Memory</x-pulse::th>
+                    <x-pulse::th class="text-right">CPU</x-pulse::th>
+                </tr>
+            </x-pulse::thead>
 
-                <tbody>
-                    @foreach ($apps as $app)
+            <tbody>
+                @foreach ($apps as $app)
 
-                    <tr wire:key="{{ $app['app'] }}">
-                        <x-pulse::td class="max-w-[1px]">
-                            <code class="block text-xs text-gray-900 dark:text-gray-100 truncate" title="{{ $app['app'] }}">
-                                {{ $app['app'] }}
-                            </code>
-                        </x-pulse::td>
+                <tr wire:key="{{ $app['app'] }}">
+                    <x-pulse::td class="max-w-[1px]">
+                        <code class="block text-xs text-gray-900 dark:text-gray-100 truncate" title="{{ $app['app'] }}">
+                            {{ $app['app'] }}
+                        </code>
+                    </x-pulse::td>
 
-                        <x-pulse::td numeric class="text-gray-700 dark:text-gray-300 font-bold">
-                            {{ $app['memory'] }} GB
-                        </x-pulse::td>
+                    <x-pulse::td numeric class="text-gray-700 dark:text-gray-300 font-bold">
+                        {{ $app['memory'] }} GB
+                    </x-pulse::td>
 
-                        <x-pulse::td numeric class="text-gray-700 dark:text-gray-300 font-bold">
-                            {{ $app['cpu'] }}%
-                        </x-pulse::td>
-                    </tr>
+                    <x-pulse::td numeric class="text-gray-700 dark:text-gray-300 font-bold">
+                        {{ $app['cpu'] }}%
+                    </x-pulse::td>
+                </tr>
 
-                    @endforeach
-                </tbody>
-            </x-pulse::table>
+                @endforeach
+            </tbody>
+        </x-pulse::table>
 
         @endif
     </x-pulse::scroll>
