14137f1f0e499b6384fab423531bbb9509280e44
Lito <lito@eordes.com>
2024-02-05 13:37:36 +0100

    Improved Card Layout

--- resources/views/livewire/card.blade.php
+++ resources/views/livewire/card.blade.php
@@ -9,14 +9,13 @@
         </x-slot:actions>
     </x-pulse::card-header>
 
-    <x-pulse::scroll :expand="$expand" wire:poll.5s="">
+    <x-pulse::scroll :expand="$expand" wire:poll.1m="">
         @if (empty($apps))
 
         <x-pulse::no-results />
 
         @else
 
-        <div class="grid grid-cols-1 @lg:grid-cols-2 @3xl:grid-cols-3 @6xl:grid-cols-4 gap-2">
             <x-pulse::table>
                 <colgroup>
                     <col width="100%" />
@@ -37,7 +36,7 @@
 
                     <tr wire:key="{{ $app['app'] }}">
                         <x-pulse::td class="max-w-[1px]">
-                            <code class="block text-xs text-gray-900 dark:text-gray-100 truncate" title="">
+                            <code class="block text-xs text-gray-900 dark:text-gray-100 truncate" title="{{ $app['app'] }}">
                                 {{ $app['app'] }}
                             </code>
                         </x-pulse::td>
@@ -54,7 +53,6 @@
                     @endforeach
                 </tbody>
             </x-pulse::table>
-        </div>
 
         @endif
     </x-pulse::scroll>
